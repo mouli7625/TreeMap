d64d9c380b7f7a870641b4cc9e0e85692a3115a0
mouli7625 <moulibhanagar07@gmail.com>
2022-07-25 11:15:37 +0530

    commit-m"Prog5"

---
+++ Prog5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Prog5 
+{
+    public static void main(String[]args)
+    {
+        TreeMap<Integer,String>map=new TreeMap<>();
+        int i;
+        map.put(1, "Mouli");
+        map.put(4,"CSE");
+        map.put(2, "Bhtangar");
+        System.out.println(map.keySet());
+
+    }
+    
+}
